8d86855c524190bb0f8e0fb39cba870b3db179a5
GemechisUrgessa <gemechisurgessa@gmail.com>
2023-01-10 16:05:09 +0300

    add-new-document

---
+++ src/pages/add-document-page.js
@@ -0,0 +1,11 @@
+import '../styles/add-document-page.css'
+const AddDocumentPage = () => {
+    return (
+        <div className="container-add-document" >
+            <h2>Add New document</h2>
+            
+        </div>
+    )
+}
+
+export default AddDocumentPage;
