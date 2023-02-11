f9f17accd808d4a03af30598ec579e1b4ad08525
unknown <jebessadejene2021@gmail.com>
2023-02-11 16:49:10 +0300

    login ui

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Box, Container } from '@mui/material';
 import MainPage from './pages/Main-page';
 import React from "react";
 import { padding } from '@mui/system';
-import { ImportantDevices } from '@mui/icons-material';
+import { ImportantDevices} from '@mui/icons-material';
+import Login from './pages/login';
 // "#f8f8f8
 function App() {
   return (
       // <Container sx={{backgroundColor:"red"}}>
       <Box minHeight={"100vh"}  paddingX={{xs:"1em",lg:"14em"}} bgcolor={{ xs:"#f8f8f8",md:"#daf2f8"}}>
-        <MainPage />
+        {/* <MainPage /> */}
+        <Login/>
+        
       </Box>
           
       // </Container>
