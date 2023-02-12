abb231053ff8bd0e6d2aef4b167c607bbf6cf0ba
zeamanual <zeamanualfeleke@gmail.com>
2023-02-12 17:18:28 +0300

    unneccessary comments removed

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import { Box, Container } from '@mui/material';
 import MainPage from './pages/main-page';
 import React from "react";
 import { padding } from '@mui/system';
-import { ImportantDevices} from '@mui/icons-material';
+import { ImportantDevices } from '@mui/icons-material';
 import Login from './pages/login';
 import PageRouter from "./router/router";
-// "#f8f8f8
+
 function App() {
   return (
-      // <Container sx={{backgroundColor:"red"}}>
-      <Box minHeight={"100vh"}  paddingX={{xs:"1em",lg:"14em"}} bgcolor={{ xs:"#f8f8f8",md:"#daf2f8"}}>
-        <PageRouter/>
-      </Box>
-          
-      // </Container>
+    <Box minHeight={"100vh"} paddingX={{ xs: "1em", lg: "14em" }} bgcolor={{ xs: "#f8f8f8", md: "#daf2f8" }}>
+      <PageRouter />
+    </Box>
   );
 }
 
