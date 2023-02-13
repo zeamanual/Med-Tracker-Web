ca7ed4c78f2f4f8eab65a95562799a740d9d7905
unknown <jebessadejene2021@gmail.com>
2023-02-13 22:22:32 +0300

    debug on faq page

--- src/pages/faq-page.jsx
+++ src/pages/faq-page.jsx
@@ -1,8 +1,6 @@
 import CustomizedAccordions from "../components/UI-components/accordion";
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
 import faq_data from "../service/faq-data.json";
