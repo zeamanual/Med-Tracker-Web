b869065d9109c763422d5634c171967cdd0eb93d
GemechisUrgessa <gemechisurgessa@gmail.com>
2023-02-14 11:27:15 +0300

    updated

--- src/state/slices/new-document.js
+++ src/state/slices/new-document.js
@@ -1,16 +1,15 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import upload from "../../service/new-document";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import  upload from '../../service/new-document';
 
 const initialState = {
-  uploadData: {
-    pendingUploads: false,
-    errorMessage: "",
-    successUploads: false,
-  },
-};
+    uploadData: {
+        pendingUploads: false,
+        errorMessage: '',
+        successUploads: false,
+    }
+}
 
 export const uploadData = createAsyncThunk(
-<<<<<<< HEAD
     'newDocument/upload',
     async ({document, documentTitle, documentType, description, enqueueSnackbar}, thunkApi) => {
         try {
@@ -27,54 +26,38 @@ export const uploadData = createAsyncThunk(
             const errorMessage = error.message
             return thunkApi.rejectWithValue(errorMessage);
         }
-=======
-  "newDocument/upload",
-  async ({ file, documentTitle, documentType, Description }, thunkApi) => {
-    try {
-      let response = await upload(
-        file,
-        documentTitle,
-        documentType,
-        Description
-      );
-      console.log(response);
-      return response.data;
-    } catch (error) {
-      const errorMessage = error.message;
-      return thunkApi.rejectWithValue(errorMessage);
->>>>>>> main
     }
-  }
-);
+)
 let addDocumentSlice = createSlice({
-  name: "newDocument",
-  initialState,
-  reducers: {
-    resetStatus: (state) => {
-      state.uploadData.pendingUploads = false;
-      state.uploadData.errorMessage = "";
-      state.uploadData.successUploads = false;
+    name: 'newDocument',
+    initialState,
+    reducers: {
+        resetStatus:(state)=>{
+            state.uploadData.pendingUploads=false
+            state.uploadData.errorMessage=''
+            state.uploadData.successUploads=false
+            
+        }
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(uploadData.pending, (state) => {
-      state.uploadData.pendingUploads = true;
-      state.uploadData.errorMessage = "";
-      state.uploadData.successUploads = true;
-    });
+    extraReducers: (builder) => {
+        builder.addCase(uploadData.pending, (state)=>{
+            state.uploadData.pendingUploads=true
+            state.uploadData.errorMessage=''
+            state.uploadData.successUploads=true
+        })
 
-    builder.addCase(uploadData.fulfilled, (state, action) => {
-      state.uploadData.pendingUploads = false;
-      state.uploadData.errorMessage = "";
-      state.uploadData.successUploads = true;
-    });
+        builder.addCase(uploadData.fulfilled, (state,action)=>{
+            state.uploadData.pendingUploads=false
+            state.uploadData.errorMessage=''
+            state.uploadData.successUploads=true
+        })
 
-    builder.addCase(uploadData.rejected, (state, action) => {
-      state.uploadData.pendingUploads = false;
-      state.uploadData.errorMessage = action.payload;
-      state.uploadData.successUploads = false;
-    });
-  },
-});
-export const { resetStatus } = addDocumentSlice.actions;
-export default addDocumentSlice.reducer;
+        builder.addCase(uploadData.rejected, (state,action)=>{
+            state.uploadData.pendingUploads=false
+            state.uploadData.errorMessage=action.payload
+            state.uploadData.successUploads=false
+        })
+    }
+})
+export const {resetStatus} = addDocumentSlice.actions
+export default addDocumentSlice.reducer
