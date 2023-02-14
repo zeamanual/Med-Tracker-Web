118a6d87128f1f26ace4dbc0e89a36b31f9bb486
unknown <jebessadejene2021@gmail.com>
2023-02-14 12:11:02 +0300

    router edited

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://5933-2a0d-5600-41-9000-00-5421.ngrok.io/";
+export const BASE_URL = "http://0776-2a0d-5600-41-9000-00-5421.ngrok.io/";
 
 let token = localStorage.getItem("token");
 
