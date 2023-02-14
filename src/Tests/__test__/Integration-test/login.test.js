82ee7e8efc27a1c20e95ac09e38dc13d6fcab153
GetnetAdugna <getnetadugna25@gmail.com>
2023-02-14 13:34:07 +0300

    adding the tests

---
+++ src/Tests/__test__/Integration-test/login.test.js
@@ -0,0 +1,39 @@
+// import React from 'react';
+// // import TestRenderer from 'react-test-renderer';
+// import { mount } from 'enzyme';
+// import LoginForm from '../../../pages/login'
+
+
+// describe('Login page integration', () => {
+
+//   test('it should render the form fields and submit button', () =>  {
+   
+//     const wrapper = mount(<LoginForm/>);
+//     const num=4;
+    
+//     let wrapperTesterNumbercount=(a,b)=>a+b;
+
+//     expect(wrapperTesterNumbercount(10,10)).toBe(20);
+//     // expect(wrapperTesterNumbercount.find('#password').length).toBe(1);
+//     // expect(wrapperTesterNumbercount.find('type="submit"').length).toBe(1);
+
+//   });
+
+// // //   test('it should accept a username and password as props', () => {
+// // //     const values = { username: "abc123", password: "password123" };
+// // //     const wrapper = mount(<LoginForm values={values} />);
+
+// // //     expect(wrapper.find('#username').value).toBe("abc123");
+// // //     expect(wrapper.find('#password').value).toBe("password123");
+
+// // //   });
+
+// // //   test('it should render an error message when an invalid credentials are entered', () => {
+// // //      const values = { username: "abc123", password: "incorrectPassword" };      // invalid credentials
+// // //      const wrapper = mount(<LoginForm values={values}/>);
+
+// // //      expect(wrapper.find('.error-message').length).toBeGreaterThanOrEqualTo(1 );  // at least one error message should be rendered
+
+// // //    });  
+
+//  });
