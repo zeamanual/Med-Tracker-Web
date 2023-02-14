482ac356cba128fc2934f34a08ca4b0d5beeddc5
GemechisUrgessa <gemechisurgessa@gmail.com>
2023-02-14 11:25:54 +0300

    updated

--- src/service/new-document.js
+++ src/service/new-document.js
@@ -1,14 +1,7 @@
 import { clientInstance } from "../config/config";
 
-const upload = (file, documentTitle, documentType, description) => {
-  let formData = new FormData();
 
-  formData.append("File", file);
-  formData.append("Title", documentTitle);
-  formData.append("Category", documentType);
-  formData.append("Description", description);
 
-<<<<<<< HEAD
   const upload = (document, documentTitle, documentType, description) =>{
     let formData = new FormData();
     console.log("upload")
@@ -26,13 +19,3 @@ const upload = (file, documentTitle, documentType, description) => {
 
 
 export default upload
-=======
-  return clientInstance.post(`/api/Document`, formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
-};
-
-export default upload;
->>>>>>> main
